c40f711c77b10175c7a2b3ddbeae43f07ea91d70
opalenzuela <opalenzuela@gmail.com>
2015-12-18 10:46:07 +0100

    Starting with PHP

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<title>Nebula</title>
+		<link href="main.css" rel="stylesheet" />
+		<script src="main.js"></script>
+	</head>
+	<body>
+		<?php if (true) { ?>
+		<h1>Nebula</h1>
+		<section>
+			<h2>Application installed</h2>
+			<p>The application has been successfully installed.</p>
+		</section>
+		<?php } ?>
+	</body>
+</html>
